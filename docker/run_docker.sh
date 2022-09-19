d71523567a9d5e79f395fc643f6513e1aae44403
wjdwlsdn1216 <wjdwlsdn1216@naver.com>
2022-09-19 21:33:13 +0900

    update docker

---
+++ docker/run_docker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo xhost +local:root
+
+sudo docker pull nvcr.io/nvidia/pytorch:21.08-py3
+
+container_name="roadking"
+docker_image="nvcr.io/nvidia/pytorch:21.08-py3"
+
+# xhost +
+
+echo ${container_name} | cat > ./container_name.txt
+
+sudo docker run -dit \
+--name=${container_name} \
+--net=host \
+--privileged \
+--ipc=host \
+--gpus all \
+-w /works \
+-v /home/roadking/works:/works \
+-e DISPLAY=$DISPLAY \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+-v /home/$USER/.Xauthority:/root/.Xauthority:rw \
+${docker_image} bash && \
+sudo docker cp ./container_name.txt ${container_name}:/ && \
+rm ./container_name.txt && \
+sudo docker exec -it ${container_name} bash
